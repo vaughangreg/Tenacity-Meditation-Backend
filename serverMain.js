33c97b69f4f00447e2fa04b6e4d8014c99c42256
Greg Vaughan <vaughan.greg@gmail.com>
2012-07-24 17:05:49 -0500

    converted configure page from jade to ejs

--- serverMain.js
+++ serverMain.js
@@ -10,13 +10,13 @@ var express = require('express'),
   	http = require('http');
 
 var tenConfig = require ('./tenconfigure.js');
-
+var ejs = require('ejs');
 var app = express();
 
 app.configure(function(){
 	app.set('port', serverConfig.serverSettings.node.port);
 	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
+	app.set('view engine', 'ejs');
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
